3c55c42a26b4775e49ba2f7b3d9014c09107cdc2
BethanyKeplinger <93795777+BethanyKeplinger@users.noreply.github.com>
2022-06-08 11:20:08 -0400

    pie chart test

--- src/EventGenre.js
+++ src/EventGenre.js
@@ -1,3 +1,4 @@
+
 import React, { useEffect, useState } from 'react';
 import { PieChart, Pie, Cell, ResponsiveContainer } from 'recharts';
 
@@ -6,34 +7,35 @@ const EventGenre = ({ events }) => {
 
     const COLORS = ['c40c06', '#e0792f', '##e0d22f', '##19b52c', '#2522d4'];
 
-    useEffect(() => {
-        const getData = () => {
-            const genres = ['React', 'JavaScript', 'Node', 'jQuery', 'AngularJS'];
-            const data = genres.map((genre) => {
-                const value = events.filter(({ summary }) =>
-                    summary.split(' ').includes(genre)
-                ).length;
+    // useEffect(() => {
+    //     const getData = () => {
+    //         const genres = ['React', 'JavaScript', 'Node', 'jQuery', 'AngularJS'];
+    //         const data = genres.map((genre) => {
+    //             const value = events.filter(({ summary }) =>
+    //                 summary.split(' ').includes(genre)
+    //             ).length;
 
-                return { name: genre, value };
-            });
-            return data;
-        };
+    //             return { name: genre, value };
+    //         });
+    //         return data;
+    //     };
+    //     setData(() => getData());
+    // }, [events]);
+
+    useEffect(() => {
         setData(() => getData());
     }, [events]);
 
-
-
-    // const RADIAN = Math.PI / 180;
-    // const renderCustomizedLabel = ({ cx, cy, midAngle, innerRadius, outerRadius, percent, index }) => {
-    //   const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
-    //   const x = cx + radius * Math.cos(-midAngle * RADIAN);
-    //   const y = cy + radius * Math.sin(-midAngle * RADIAN);
-
-    //     <text x={x} y={y} fill="white" textAnchor={x > cx ? 'start' : 'end'} dominantBaseline="central">
-    //       {`${(percent * 100).toFixed(0)}%`}
-    //     </text>
-    //   );
-    // };
+    const getData = () => {
+        const genres = ['React', 'JavaScript', 'Node', 'jQuery', 'AngularJS'];
+        const data = genres.map((genre) => {
+            const value = events.filter((event) =>
+                event.summary.split(' ').include(genre)
+            ).length;
+            return { name: genre, value };
+        });
+        return data;
+    };
 
     return (
         <ResponsiveContainer height="400">
